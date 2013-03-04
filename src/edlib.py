fd3f9fb52823b6eade46d5be19596eb0a20aafc8
Bill Eger <egeriicw@gmail.com>
2013-03-04 14:00:16 -0500

    initial commit

---
+++ src/edlib.py
@@ -0,0 +1,20 @@
+import csv
+import json
+import numpy as np
+import pandas as pd
+
+class EnergyData():
+
+    def __init__(self):
+        self.name = ""
+        self.date = ""
+        self.__version__ = "v0.1"
+
+
+def main():
+    print "Main()"
+    
+
+
+if __name__ == "__main__":
+    main()
